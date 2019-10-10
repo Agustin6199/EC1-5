974b9a5311b1e7b5dbe2ce799622deb4357a20fe
Gerole10 <gerole10@gmail.com>
2019-10-10 08:57:03 +0200

    Creación de la clase Estado

---
+++ src/practicassisinteligentes/Estado.java
@@ -0,0 +1,6 @@
+package practicassisinteligentes;
+
+public class Estado {
+    //Un cubo y una EDA almacenando los demás cubos
+    //Dos constructores, uno a partir de un cubo y otro a partir de un json
+}
